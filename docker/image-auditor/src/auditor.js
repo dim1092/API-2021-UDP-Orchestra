15ff7079c289c36076761784e259f38fc1d1d7a4
Dimitri <dimitri.debleser@heig-vd.ch>
2022-02-03 10:03:46 +0100

    update

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -3,20 +3,19 @@ var musicians = new Map(); // To store our musicians
 // Instrument / sound map
 const instrumentMap = new Map(
     [
-        ["piano", "ti-ta-ti"],
-        ["trumpet", "pouet"],
-        ["flute", "trulu"],
-        ["violin", "gzi-gzi"],
-        ["drum", "boum-boum"]
+        ["ti-ta-ti", "piano"],
+        ["pouet", "trumpet"],
+        ["trulu", "flute"],
+        ["gzi-gzi", "violin"],
+        ["boum-boum", "drum"]
     ]
 )
-
 
 // -------------- Listening on UDP for new sounds --------------
 var dgram = require('dgram'); // We use a standard Node.js module to work with UDP
 var socket = dgram.createSocket('udp4'); // Create datagram socket
 
-// bind to socket and listen need to join multicast group
+// bind to socket and listen, need to join multicast group
 socket.bind(protocol.PROTOCOL_PORT, function() {
     console.log("Joining multicast group");
     socket.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS);
@@ -24,15 +23,16 @@ socket.bind(protocol.PROTOCOL_PORT, function() {
 
 // callback for received datagram
 socket.on('message', function(msg, source) {
-    console.log("Received sound sound");
-
     let sound = JSON.parse(msg); // Parsing musician object
+    console.log("Received sound : " + sound.sound);
     if (!musicians.has(sound.uuid)) {
-        let musician = new Object();
+        let struct = {};
+        let musician = {};
         musician.uuid = sound.uuid;
         musician.instrument = instrumentMap.get(sound.sound);
         musician.activeSince = Date.now(); // Adding attribute
-        musicians.set(sound.uuid, musician); // Adding to our musicians list if new
+        struct.musician = musician;
+        musicians.set(sound.uuid, struct); // Adding to our musicians list if new
     }
     musicians.get(sound.uuid).lastHeard = Date.now(); // Update last time active
 });
@@ -47,15 +47,17 @@ tcpServer.listen(protocol.PROTOCOL_PORT);
 
 // When a connection is established send active musicians
 tcpServer.on('connection', function (socket) {
+    console.log("TCP : connected")
     let activeMusicians = new Array(0);
-    musicians.forEach(musician => function() {
-        if (Date.now() - musician.lastHeard < 5000) {
-            activeMusicians.push(musician);
+    // Adding active musician in the array
+    musicians.forEach(function(value, key, map) {
+        if (Date.now() - value.lastHeard < 5000) {
+            activeMusicians.push(value.musician);
         }
     });
 
     let message = JSON.stringify(activeMusicians);
     let buf = new Buffer(message);
     socket.write(buf);
-    socket.close();
+    socket.end();
 })
